c501806ac2e9d5f5ddd9776dfdff9a4da5045036
searchme86 <searchme86@gmail.com>
2022-06-08 16:28:13 +0900

    base structure

--- src/Config/Layout.jsx
+++ src/Config/Layout.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+// import Navbar from '../Components/Header/Nav/Navbar';
+// import SearchBox from '../Components/SeachBar/SearchBox';
+// import Slider from '../Components/Slider/Slider';
 
 function Layout() {
   return (
     <div className="App">
-      <Outlet />
+      {/* <SearchBox />
+      <Navbar />
+      <Slider autoPlay={2} /> */}
+      <main className="">
+        <Outlet />
+      </main>
     </div>
   );
 }
