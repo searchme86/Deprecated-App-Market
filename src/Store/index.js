79f22c9e3e3448d4db51c12ca9823c5cd5bcb29b
searchme86 <searchme86@gmail.com>
2022-06-13 14:32:32 +0900

    ✨ 통합 리듀서 : 상품 리듀서(productSlice) 등록

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 // import AuthReducer from './features/authSlice';
 // import TourReducer from './features/tourSlice';
 import AuthReducer from './Features/AuthSlice';
-import CartSlice from './Features/CartSlice';
+import CartReducer from './Features/CartSlice';
 import TourReducer from './Features/TourSlice';
+import ProductReducer from './Features/ProductSlice';
 
 export default configureStore({
   reducer: {
     auth: AuthReducer,
     tour: TourReducer,
-    cart: CartSlice,
+    cart: CartReducer,
+    product: ProductReducer,
   },
 });
