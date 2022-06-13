f3cda8f6a99553806632c702b526e28108437058
searchme86 <searchme86@gmail.com>
2022-06-13 10:27:22 +0900

    ✨ Cart redux added

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 // import AuthReducer from './features/authSlice';
 // import TourReducer from './features/tourSlice';
 import AuthReducer from './Features/AuthSlice';
+import CartSlice from './Features/CartSlice';
 import TourReducer from './Features/TourSlice';
 
 export default configureStore({
   reducer: {
     auth: AuthReducer,
     tour: TourReducer,
+    cart: CartSlice,
   },
 });
