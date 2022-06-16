902a7838d56de1ff969c049a2ac24e87cb32efac
searchme86 <searchme86@gmail.com>
2022-06-16 14:55:35 +0900

    🎨  모달 컴포넌트를 담는 컴포넌트 추가함

---
+++ src/Pages/Manager/category/CategoryModal/CategoryModal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ModalFrame from '../../../../Components/Modal/ModalFrame';
+
+function CategoryModal({ handleClose, isOpen }) {
+  return (
+    <ModalFrame handleClose={handleClose} isOpen={isOpen}>
+      <>
+        <span>ddd</span>
+      </>
+    </ModalFrame>
+  );
+}
+
+export default CategoryModal;
