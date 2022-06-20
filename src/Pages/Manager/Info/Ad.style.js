c288fc3957bac1eed335d68fc5484e66c23dc717
searchme86 <searchme86@gmail.com>
2022-06-20 21:31:38 +0900

    ✨ 메인 페이지 상단 헤더 섹션 추가

---
+++ src/Pages/Manager/Info/Ad.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const MainAdContainer = styled.div`
+  position: relative;
+`;
+
+export const MainAdHeader = styled.div``;
+
+export const MainAdHeaderTitle = styled.strong`
+  display: block;
+  font-size: 64px;
+  margin: 80px 0 20px 0;
+`;
+
+export const MainAdContent = styled.div``;
+
+export const MainAdDes = styled.p`
+  font-size: 21px;
+  color: #737373;
+`;
+
+export const MainAdFunction = styled.div``;
