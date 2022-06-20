1d52d0f9a566b24afa624638b2ef0de0964fdd6b
searchme86 <searchme86@gmail.com>
2022-06-20 21:34:38 +0900

    💄 게시물 수정을 위한 공통 버튼 추가

--- src/Config/Styles/CommonStyle.js
+++ src/Config/Styles/CommonStyle.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const TextHidden = styled.p`
@@ -9,4 +10,18 @@ export const TextHidden = styled.p`
   z-index: -1;
   clip: rect(1px, 1px, 1px, 1px);
   clip-path: inset(50%);
+`;
+
+export const ButtonLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 130px;
+  margin: 60px 0 0 0;
+  padding: 10px 40px;
+  font-size: 18px;
+  border-radius: 80px;
+  border: 2px solid red;
+  transition: opacity 0.1s ease-in;
+  word-break: keep-all;
 `;
