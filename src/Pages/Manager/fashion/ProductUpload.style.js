0bd8c51e4ecb01e08ebe2ff7a36b89aab3fabe81
searchme86 <searchme86@gmail.com>
2022-06-27 23:22:10 +0900

    ⚡️ 상품등록 페이지에 태그 컴포넌트 추가 및 코드 수정

--- src/Pages/Manager/fashion/ProductUpload.style.js
+++ src/Pages/Manager/fashion/ProductUpload.style.js
@@ -125,3 +125,49 @@ export const PFormDes = styled.p`
   font-size: 12px;
   color: #767676;
 `;
+
+export const TagWrapper = styled.div``;
+
+export const TagContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  border: 1px solid rgb(226, 232, 240);
+  padding: 0.5em;
+  border-radius: 6px;
+`;
+
+export const TagItemList = styled.ul``;
+
+export const TagItem = styled.li`
+  background-color: rgb(218, 216, 216);
+  display: inline-block;
+  padding: 0.5em 0.75em;
+  border-radius: 20px;
+  margin: 5px 5px 0 0;
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+export const TagItemDelete = styled.span`
+  height: 20px;
+  width: 20px;
+  background-color: #767676;
+  color: #fff;
+  border-radius: 50%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 0.5em;
+  font-size: 18px;
+  cursor: pointer;
+`;
+
+export const TagInput = styled.input`
+  flex-grow: 1;
+  padding: 0.5em 0;
+  border: none;
+  outline: none;
+  margin: 5px 0 0 0;
+`;
