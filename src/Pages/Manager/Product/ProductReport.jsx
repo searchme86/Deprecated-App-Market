57adb0db6928ff28f755767a4dc92d38543e3925
searchme86 <searchme86@gmail.com>
2022-06-29 13:44:40 +0900

    🚚  상위폴더명 변경에 따른 파일 새로커밋

---
+++ src/Pages/Manager/Product/ProductReport.jsx
@@ -0,0 +1,321 @@
+import React from 'react';
+import {
+  ModalContent,
+  ModalForm,
+  ModalHeader,
+  ModalTitle,
+} from '../../../Components/Modal/Modal.style';
+import ModalFrame from '../../../Components/Modal/ModalFrame';
+import { useSelector } from 'react-redux';
+import { Image, ImageHolder } from '../../../Assets/Styles/Image.style';
+import {
+  ReportAlert,
+  ReportAlertList,
+  ReportAlertMessage,
+  ReportContent,
+  ReportDOpinion,
+  ReportDTitle,
+  ReportHeader,
+  ReportImage,
+  ReportInfo,
+  ReportInfoItem,
+  ReportInfoLi,
+  ReportInfoList,
+  ReportIntro,
+  ReportMain,
+  ReportMore,
+  ReportOverflow,
+  ReportOverflowLi,
+  ReportOverflowList,
+  ReportTitle,
+  ReportUser,
+  ReportBlank,
+  RTable,
+  RTableTd,
+  RTableTh,
+  RTableWrapper,
+} from './ProductReport.style';
+import { OffScreenSpan } from '../../../Assets/Styles/Basic.style';
+import { AlignList } from '../../../Assets/Styles/Layout.style';
+import defaultImg from '../../../Assets/Image/default-product-upload.png';
+
+function ProductReport({ prReport }) {
+  const { handleClose, isOpen, newProduct } = prReport;
+
+  const {
+    user: {
+      result: { nickname },
+    },
+  } = useSelector((state) => state.auth);
+  const {
+    pdCategory,
+    pdTitle,
+    pdImage,
+    pdPrice,
+    pdDes,
+    pdWish,
+    prdDegree,
+    pdStatus,
+    pdSizeInfo,
+    pdColorInfo,
+  } = newProduct;
+
+  const [{ pdSize, pdPriceBySize }] = pdSizeInfo;
+  const [{ pdColor, pdPriceByColor }] = pdColorInfo;
+
+  const isBlank = [
+    pdCategory,
+    pdTitle,
+    pdImage,
+    pdPrice,
+    pdDes,
+    pdWish,
+    prdDegree,
+    pdStatus,
+  ].every(Boolean);
+
+  return (
+    <div>
+      <ModalFrame
+        handleClose={handleClose}
+        isOpen={isOpen}
+        domId="product-report-modal"
+      >
+        <>
+          <ModalHeader>
+            <ModalTitle>등록상품 미리보기</ModalTitle>
+          </ModalHeader>
+          <ModalContent>
+            <ModalForm>
+              <ReportContent>
+                <ReportHeader>
+                  {!isBlank && (
+                    <ReportAlert>
+                      <ReportAlertList>
+                        <ReportAlertMessage>
+                          * 상품 등록 후, 미리보기가 가능합니다
+                        </ReportAlertMessage>
+                        <ReportAlertMessage>
+                          * 미 등록된 항목은 "공란" 이란 텍스트로 표현됩니다.
+                        </ReportAlertMessage>
+                      </ReportAlertList>
+                    </ReportAlert>
+                  )}
+                  <ReportIntro>
+                    <ReportUser>{nickname}</ReportUser>
+                    님이 등록하시는 상품 요약입니다.
+                  </ReportIntro>
+                </ReportHeader>
+                <ReportMain>
+                  <ReportImage>
+                    <ImageHolder>
+                      <Image
+                        src={pdImage ? pdImage : defaultImg}
+                        alt={`${pdTitle} 이미지`}
+                      />
+                    </ImageHolder>
+                  </ReportImage>
+                  <ReportInfo>
+                    <ReportInfoList>
+                      <ReportInfoLi>
+                        <ReportTitle>카테고리</ReportTitle>
+                        <ReportDTitle>
+                          {!isBlank ? (
+                            <ReportBlank>공란</ReportBlank>
+                          ) : (
+                            pdCategory
+                          )}
+                        </ReportDTitle>
+                      </ReportInfoLi>
+                      <ReportInfoLi>
+                        <AlignList>
+                          <ReportInfoItem>
+                            <ReportTitle>상품명</ReportTitle>
+                            <ReportDTitle>
+                              {!isBlank ? (
+                                <ReportBlank>공란</ReportBlank>
+                              ) : (
+                                pdTitle
+                              )}
+                            </ReportDTitle>
+                          </ReportInfoItem>
+                          <ReportInfoItem>
+                            <ReportTitle>상품가격</ReportTitle>
+                            <ReportDTitle>{`${Number(pdPrice).toLocaleString(
+                              'ko-KR'
+                            )} 원`}</ReportDTitle>
+                          </ReportInfoItem>
+                          <ReportInfoItem>
+                            <ReportTitle>상품단계</ReportTitle>
+                            <ReportDTitle>
+                              {!isBlank ? (
+                                <ReportBlank>공란</ReportBlank>
+                              ) : (
+                                prdDegree
+                              )}
+                            </ReportDTitle>
+                          </ReportInfoItem>
+                        </AlignList>
+                      </ReportInfoLi>
+                      <ReportInfoLi>
+                        <ReportTitle>상품소개</ReportTitle>
+                        <ReportOverflow>
+                          <ReportDOpinion>
+                            {!isBlank ? <ReportBlank>공란</ReportBlank> : pdDes}
+                          </ReportDOpinion>
+                        </ReportOverflow>
+                      </ReportInfoLi>
+                      <ReportInfoLi>
+                        <ReportTitle>상품상태 설명</ReportTitle>
+                        <ReportOverflow>
+                          <ReportOverflowList>
+                            {pdStatus.map((status, index) => (
+                              <ReportOverflowLi key={index}>
+                                <ReportDOpinion>
+                                  {!isBlank ? (
+                                    <ReportBlank>공란</ReportBlank>
+                                  ) : (
+                                    status
+                                  )}
+                                </ReportDOpinion>
+                              </ReportOverflowLi>
+                            ))}
+                          </ReportOverflowList>
+                        </ReportOverflow>
+                      </ReportInfoLi>
+                      <ReportInfoLi>
+                        <ReportTitle>희망사항</ReportTitle>
+                        <ReportOverflow>
+                          <ReportDOpinion>
+                            {!isBlank ? (
+                              <ReportBlank>공란</ReportBlank>
+                            ) : (
+                              pdWish
+                            )}
+                          </ReportDOpinion>
+                        </ReportOverflow>
+                      </ReportInfoLi>
+                    </ReportInfoList>
+                  </ReportInfo>
+                </ReportMain>
+                <ReportMore>
+                  {!isBlank ? (
+                    <AlignList>
+                      <ReportInfoLi>
+                        <ReportTitle>사이즈 별 가격</ReportTitle>
+                        <ReportBlank>공란</ReportBlank>
+                      </ReportInfoLi>
+                      <ReportInfoLi>
+                        <ReportTitle>색상 별 가격</ReportTitle>
+                        <ReportBlank>공란</ReportBlank>
+                      </ReportInfoLi>
+                    </AlignList>
+                  ) : (
+                    <AlignList>
+                      <ReportInfoLi>
+                        {pdSize && pdPriceBySize && (
+                          <>
+                            <ReportTitle>사이즈 별 가격</ReportTitle>
+                            <RTableWrapper>
+                              <RTable>
+                                <caption>
+                                  <OffScreenSpan>
+                                    사이즈 별 가격 테이블
+                                  </OffScreenSpan>
+                                </caption>
+                                <colgroup>
+                                  <col
+                                    style={{
+                                      width: '50%',
+                                    }}
+                                  />
+                                  <col style={{ width: '50%' }} />
+                                </colgroup>
+                                <thead>
+                                  <tr>
+                                    <RTableTh scope="col">상품 사이즈</RTableTh>
+                                    <RTableTh scope="col">가격</RTableTh>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {pdSizeInfo.map(
+                                    ({ pdSize, pdPriceBySize }, index) => (
+                                      <tr key={index}>
+                                        <RTableTh scope="row">
+                                          {pdSize}
+                                        </RTableTh>
+                                        <RTableTd>
+                                          {`${Number(
+                                            pdPriceBySize
+                                          ).toLocaleString('ko-KR')} 원`}
+                                        </RTableTd>
+                                      </tr>
+                                    )
+                                  )}
+                                </tbody>
+                              </RTable>
+                            </RTableWrapper>
+                          </>
+                        )}
+                      </ReportInfoLi>
+                      <ReportInfoLi>
+                        <>
+                          {pdColor && pdPriceByColor && (
+                            <ReportInfoLi>
+                              <ReportTitle>색상 별 가격</ReportTitle>
+                              <RTableWrapper>
+                                <RTable>
+                                  <caption>
+                                    <OffScreenSpan>
+                                      색상 별 가격 테이블
+                                    </OffScreenSpan>
+                                  </caption>
+                                  <colgroup>
+                                    <col
+                                      style={{
+                                        width: '50%',
+                                      }}
+                                    />
+                                    <col style={{ width: '50%' }} />
+                                  </colgroup>
+                                  <thead>
+                                    <tr>
+                                      <RTableTh scope="col">상품 색상</RTableTh>
+                                      <RTableTh scope="col">가격</RTableTh>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    {pdColorInfo.map(
+                                      ({ pdColor, pdPriceByColor }, index) => (
+                                        <tr key={index}>
+                                          <RTableTh scope="row">
+                                            {pdColor}
+                                          </RTableTh>
+                                          <RTableTd>
+                                            {`${Number(
+                                              pdPriceByColor
+                                            ).toLocaleString('ko-KR')} 원`}
+                                          </RTableTd>
+                                        </tr>
+                                      )
+                                    )}
+                                  </tbody>
+                                </RTable>
+                              </RTableWrapper>
+                            </ReportInfoLi>
+                          )}
+                        </>
+                      </ReportInfoLi>
+                    </AlignList>
+                  )}
+                </ReportMore>
+              </ReportContent>
+            </ModalForm>
+          </ModalContent>
+        </>
+      </ModalFrame>
+    </div>
+  );
+}
+
+export default ProductReport;
---
+++ src/Pages/Manager/Product/ProductReport.style.js
@@ -0,0 +1,143 @@
+import styled from 'styled-components';
+
+export const ReportHeader = styled.div`
+  margin: 0 0 10px 0;
+`;
+
+export const ReportIntro = styled.p``;
+
+export const ReportUser = styled.strong`
+  display: inline-block;
+  vertical-align: center;
+  line-height: 1;
+  font-size: 20px;
+  margin: 0 5px 0 0;
+`;
+
+export const ReportContent = styled.div``;
+
+export const ReportMain = styled.div`
+  display: flex;
+`;
+
+export const ReportImage = styled.div`
+  width: 50%;
+  margin: 0 20px 0 0;
+`;
+
+export const ReportInfo = styled.div`
+  width: 50%;
+`;
+
+export const ReportInfoList = styled.ul``;
+
+export const ReportInfoLi = styled.li`
+  flex: 1;
+  margin: 10px 0 10px 0;
+  text-align: left;
+
+  &:first-child {
+    margin-top: 0;
+  }
+  &:last-child {
+    margin-top: 0;
+  }
+`;
+
+export const ReportTitle = styled.strong`
+  position: relative;
+  display: block;
+  font-size: 15px;
+  font-weight: bold;
+  padding: 0 0 0 10px;
+  transition: 0.2s;
+  &:after {
+    position: absolute;
+    content: '';
+    left: 0;
+    bottom: 0;
+    width: 3px;
+    height: 100%;
+    background: #767676;
+  }
+`;
+
+export const ReportDTitle = styled.span`
+  margin: 0 0 0 10px;
+  font-size: 14px;
+  font-weight: normal;
+  white-space: nowrap;
+  word-break: keep-all;
+`;
+
+export const ReportDOpinion = styled.p`
+  margin: 0 0 0 5px;
+  font-size: 14px;
+`;
+
+export const ReportInfoItem = styled.li`
+  flex: 1;
+  text-align: left;
+`;
+
+export const ReportOverflow = styled.div`
+  overflow-y: auto;
+  height: 60px;
+  padding: 5px;
+  box-sizing: border-box;
+`;
+
+export const ReportOverflowList = styled.ul``;
+
+export const ReportOverflowLi = styled.li`
+  margin: 0 2px 0 0;
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+export const ReportMore = styled.div`
+  overflow-y: auto;
+  height: 160px;
+  padding: 5px;
+  box-sizing: border-box;
+`;
+
+export const RTableWrapper = styled.div`
+  margin: 7px 0 0 0;
+`;
+
+export const RTable = styled.table`
+  width: 100%;
+`;
+
+export const RTableTh = styled.th`
+  font-size: 14px;
+  text-align: center;
+`;
+
+export const RTableTd = styled.td`
+  font-size: 14px;
+  text-align: center;
+`;
+
+export const ReportAlert = styled.ul``;
+
+export const ReportAlertList = styled.li`
+  margin: 0 0 10px 0;
+`;
+
+export const ReportAlertMessage = styled.p`
+  font-size: 14px;
+  font-weight: bold;
+  color: #ea035c;
+`;
+
+export const ReportBlank = styled.span`
+  display: inline-block;
+  vertical-align: center;
+  margin: 2px 0 0 0;
+  font-size: 14px;
+  font-weight: bold;
+  color: #ea035c;
+`;
