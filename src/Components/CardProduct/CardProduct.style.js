a13ea056504060bff555a3560e6fc462943847f3
searchme86 <searchme86@gmail.com>
2022-07-04 18:25:40 +0900

    💄 카드컴포넌트 스타일 수정

--- src/Components/CardProduct/CardProduct.style.js
+++ src/Components/CardProduct/CardProduct.style.js
@@ -15,7 +15,6 @@ export const PCardItem = styled.li`
   &:last-child {
     margin-right: 0;
   }
-  background: red;
 `;
 
 export const PCardIspace = styled.span`
@@ -23,3 +22,60 @@ export const PCardIspace = styled.span`
   padding: 0;
   padding-top: 56.25%;
 `;
+
+export const PCardCategory = styled.span`
+  display: block;
+  width: 49px;
+  background: red;
+  font-size: 15px;
+  color: #fff;
+  text-align: center;
+  white-space: nowrap;
+  word-break: keep-all;
+`;
+
+export const PCardPrice = styled.strong`
+  display: block;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: left;
+  margin: 5px 0 5px 0;
+  white-space: nowrap;
+  word-break: keep-all;
+`;
+
+export const PCardTitle = styled.span`
+  display: block;
+  width: 200px;
+  color: #222;
+  margin: 0 0 5px 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+export const PCardDes = styled.p`
+  color: #222;
+  font-size: 15px;
+  margin: 0 0 5px 0;
+  height: 70px;
+  line-height: 1.5;
+  background: red;
+`;
+
+export const PCardUser = styled.div`
+  display: flex;
+`;
+
+export const PUserImage = styled.div`
+  width: 55px;
+`;
+
+export const PUserInfo = styled.div``;
+
+export const PUserNickname = styled.span`
+  display: block;
+`;
+export const PUserAddress = styled.p``;
