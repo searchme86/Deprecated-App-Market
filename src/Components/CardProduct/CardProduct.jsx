63b5f41a374073888f92461687575fdfec8ae5f6
searchme86 <searchme86@gmail.com>
2022-07-04 16:48:38 +0900

    ✨  상품 페이지의 데이터 로그 구문 추가(추후 삭제)

--- src/Components/CardProduct/CardProduct.jsx
+++ src/Components/CardProduct/CardProduct.jsx
@@ -21,14 +21,26 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { likeProduct } from '../../Store/Features/NProductSlice';
 import { Image, ImageHolder } from '../../Assets/Styles/Image.style';
-import { PCardIspace, PCardItem } from './CardProduct.style';
+import {
+  PCardIspace,
+  PCardItem,
+  PCardUser,
+  PCardCategory,
+  PUserAddress,
+  PUserImage,
+  PUserInfo,
+  PCardPrice,
+  PCardDes,
+  PCardTitle,
+} from './CardProduct.style';
 
 function CardProduct(item) {
-  console.log('cardProduct 컴포넌트에서 item', item);
+  // console.log('cardProduct 컴포넌트에서 item', item);
   const {
     _id,
     pdUploaderNickname,
     pdUploaderImage,
+    pdCategory,
     pdImage,
     pdPrice,
     pdDes,
@@ -36,24 +48,26 @@ function CardProduct(item) {
     pdtags,
     pdlikes,
     pdAddress,
+    pdStatus,
+    pdDegree,
   } = item;
-  console.log(
-    'item',
-    _id,
-    pdUploaderNickname,
-    pdUploaderImage
-    // pdUploader,
-    // pdImage,
-    // pdDes,
-    // pdTitle,
-    // pdtags,
-    // pdlikes
-  );
-  const {
-    user: {
-      result: { nickname, imageFile },
-    },
-  } = useSelector((state) => state.auth);
+  // console.log(
+  //   'item',
+  //   // _id,
+  //   // pdUploaderNickname,
+  //   // pdUploaderImage
+  //   // pdUploader,
+  //   // pdImage,
+  //   // pdDes,
+  //   // pdTitle,
+  //   // pdtags,
+  //   // pdlikes
+  // );
+  // const {
+  //   user: {
+  //     result: { nickname, imageFile },
+  //   },
+  // } = useSelector((state) => state.auth);
   const { user } = useSelector((state) => state.auth);
   const userId = user?.result?._id;
   // const userId = _id;
@@ -102,72 +116,113 @@ function CardProduct(item) {
     dispatch(likeProduct({ _id }));
   };
 
-  console.log('user', user);
-  console.log('_id', _id);
+  // console.log('user', user);
+  // console.log('_id', _id);
 
   return (
     <>
       <PCardItem>
-        <ImageHolder
-          height="150px"
-          style={{
-            position: 'relative',
-            width: 'initial',
-            height: 'initial',
-            boxSizing: 'border-box',
-            padding: '0',
-            margin: '0',
-          }}
-        >
-          <PCardIspace />
-          <Image
-            src={pdImage}
-            alt={pdTitle}
-            style={{
-              position: 'absolute',
-              width: '100%',
-              height: '100%',
-              top: '0',
-            }}
-          />
-        </ImageHolder>
-        <div className="" style={{ position: 'relative' }}>
-          <div className="">
-            <p>{pdTitle}</p>
-            <p>{`${Number(pdPrice).toLocaleString('ko-KR')} 원`}</p>
-            <p>{pdAddress}</p>
+        <div className="">
+          <Link to={`/product/${_id}`}>
+            <ImageHolder
+              height="150px"
+              style={{
+                position: 'relative',
+                width: 'initial',
+                height: 'initial',
+                boxSizing: 'border-box',
+                padding: '0',
+                margin: '0',
+              }}
+            >
+              <PCardIspace />
+              <Image
+                src={pdImage}
+                alt={pdTitle}
+                style={{
+                  position: 'absolute',
+                  width: '100%',
+                  height: '100%',
+                  top: '0',
+                }}
+              />
+            </ImageHolder>
+          </Link>
+          <div
+            className=""
+            style={{ padding: '20px', boxSizing: 'border-box' }}
+          >
             <div className="">
-              <ul>
-                {pdtags.map((tag, index) => (
-                  <li>
-                    <Link to={`/products/tag/${tag}`}>#{tag}</Link>
-                  </li>
-                ))}
-              </ul>
-              <MDBBtn
-                style={{ float: 'right' }}
-                tag="a"
-                color="none"
-                onClick={!user?.result ? null : handleLike}
-              >
-                {!user?.result ? (
-                  <MDBTooltip title="Please login to like tour" tag="a">
-                    <Likes />
-                  </MDBTooltip>
-                ) : (
-                  <Likes />
-                )}
-              </MDBBtn>
-            </div>
-            <div className="">
-              <p>{pdDes}</p>
-            </div>
-            <div className="">
+              <PCardCategory>{pdCategory}</PCardCategory>
+              <PCardPrice>
+                {`${Number(pdPrice).toLocaleString('ko-KR')}`}
+              </PCardPrice>
+              <PCardTitle>
+                <Link to={`/product/${_id}`}>{pdTitle}</Link>
+              </PCardTitle>
+              <PCardDes>{pdDes}</PCardDes>
+
               <div className="">
-                <img src={pdUploaderImage} alt={pdUploaderNickname} />
+                <ul style={{}}>
+                  {pdStatus.map((status, index) => (
+                    <li
+                      key={index}
+                      style={{
+                        display: 'inline-block',
+                        border: '1px solid red',
+                        borderRadius: '15px',
+                        marginBottom: '10px',
+                        marginRight: '5px',
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontSize: '15px',
+                          padding: '10px',
+                          boxSizing: 'border-box',
+                        }}
+                      >
+                        #{status}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <span>{pdUploaderNickname}</span>
+              <div className="">
+                <div
+                  className=""
+                  style={{ display: 'flex', justifyContent: 'space-between' }}
+                >
+                  <MDBBtn
+                    style={{ float: 'right' }}
+                    tag="a"
+                    color="none"
+                    onClick={!user?.result ? null : handleLike}
+                  >
+                    {!user?.result ? (
+                      <MDBTooltip title="Please login to like tour" tag="a">
+                        <Likes />
+                      </MDBTooltip>
+                    ) : (
+                      <Likes />
+                    )}
+                  </MDBBtn>
+                  <span>{pdDegree}</span>
+                </div>
+              </div>
             </div>
+
+            <PCardUser>
+              <PUserImage>
+                <ImageHolder br="100%">
+                  <Image src={pdUploaderImage} alt={pdUploaderNickname} />
+                </ImageHolder>
+              </PUserImage>
+              <PUserInfo>
+                <PUserAddress>{pdUploaderNickname}</PUserAddress>
+                <PUserAddress>{pdAddress}</PUserAddress>
+              </PUserInfo>
+            </PCardUser>
           </div>
         </div>
       </PCardItem>
