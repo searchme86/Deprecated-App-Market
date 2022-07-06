111a8c84bde8a30dd0b310c1358fb07ea4e7b23f
searchme86 <searchme86@gmail.com>
2022-07-06 22:52:19 +0900

    ⚰️  필요없는 코드 삭제

---
+++ src/Pages/Manager/Product/ProductDelete.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  ModalAction,
+  ModalContent,
+  ModalHeader,
+  ModalSeconDaryBtn,
+  ModalTitle,
+  ModalPrimaryBtn,
+} from '../../../Components/Modal/Modal.style';
+import ModalFrame from '../../../Components/Modal/ModalFrame';
+import { useDispatch } from 'react-redux';
+import { deleteProduct } from '../../../Store/Features/NProductSlice';
+
+function ProductDelete({ deleteModal }) {
+  const {
+    handleClose,
+    isOpen,
+    itemInfo: { name, id },
+  } = deleteModal;
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    if (!id) return;
+    console.log('itemNum', id);
+    id && dispatch(deleteProduct(id));
+    handleClose();
+  };
+
+  console.log('name', name);
+  console.log('id', id);
+
+  return (
+    <div>
+      <ModalFrame
+        handleClose={handleClose}
+        isOpen={isOpen}
+        domId="delete-product-modal"
+      >
+        <>
+          <ModalHeader>
+            <ModalTitle>상품 삭제하기</ModalTitle>
+          </ModalHeader>
+          <ModalContent>
+            <div className="">
+              <div
+                className=""
+                style={{
+                  textAlign: 'center',
+                  marginTop: '10px',
+                  marginBottom: '10px',
+                }}
+              >
+                <p style={{ fontSize: '21px' }}>
+                  {' '}
+                  상품, <strong style={{ fontSize: '21px' }}>
+                    {name}
+                  </strong>{' '}
+                  을/를 삭제하시겠습니까?
+                </p>
+              </div>
+              <ModalAction>
+                <ModalSeconDaryBtn type="button" onClick={handleClose}>
+                  취소
+                </ModalSeconDaryBtn>
+                <ModalPrimaryBtn type="button" onClick={() => handleDelete(id)}>
+                  삭제
+                </ModalPrimaryBtn>
+              </ModalAction>
+            </div>
+          </ModalContent>
+        </>
+      </ModalFrame>
+    </div>
+  );
+}
+
+export default ProductDelete;
