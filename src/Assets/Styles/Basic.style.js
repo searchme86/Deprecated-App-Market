931ecd8d8bdeeab8bd7983ea2cac66a0917b0a3e
searchme86 <searchme86@gmail.com>
2022-07-07 00:25:41 +0900

    💄 섹션타이틀을 offscreen 하기 위해 section용 offscreen 스타일추가

--- src/Assets/Styles/Basic.style.js
+++ src/Assets/Styles/Basic.style.js
@@ -22,6 +22,17 @@ export const OffScreenSpan = styled.span`
   clip-path: inset(50%);
 `;
 
+export const OffScreenSection = styled.section`
+  display: inline-block;
+  position: absolute;
+  overflow: hidden;
+  border: 0;
+  width: 1px;
+  height: 1px;
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(50%);
+`;
+
 export const BasicUl = styled.ul`
   margin: 0 auto;
 `;
