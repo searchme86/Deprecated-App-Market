177ed5442d68813a9963994fa4a2077d13e440a9
searchme86 <searchme86@gmail.com>
2022-07-08 01:56:53 +0900

    💄  공통 스켈레톤 스타일 추가

--- src/Config/Styles/CommonStyle.js
+++ src/Config/Styles/CommonStyle.js
@@ -25,3 +25,20 @@ export const ButtonLink = styled(Link)`
   transition: opacity 0.1s ease-in;
   word-break: keep-all;
 `;
+
+export const SkeletonList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const SkeletonLi = styled.li`
+  width: calc((100% - 40px) / 4);
+  height: 200px;
+  margin: 0 10px 10px 0;
+  &:nth-child(4n) {
+    margin-right: 0;
+  }
+  &:last-child {
+    margin-right: 0;
+  }
+`;
