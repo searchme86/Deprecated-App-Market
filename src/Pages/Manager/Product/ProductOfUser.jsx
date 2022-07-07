8d7b2e51866ccf1a1d7ee2e3f0bcff5809d2e1a8
searchme86 <searchme86@gmail.com>
2022-07-08 01:56:23 +0900

    ⚡️ 상품 로딩 중에 화면, 않 보이는 부분 skeleton 적용

--- src/Pages/Manager/Product/ProductOfUser.jsx
+++ src/Pages/Manager/Product/ProductOfUser.jsx
@@ -33,7 +33,7 @@ import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { CenterLayout } from '../../../Assets/Styles/Layout.style';
 import { ImageHolder, Image } from '../../../Assets/Styles/Image.style';
 import { OffScreenSpan } from '../../../Assets/Styles/Basic.style';
-import { Spinner } from '@chakra-ui/react';
+import { Spinner, Skeleton } from '@chakra-ui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
@@ -49,6 +49,7 @@ import './productOfUser.css';
 import { Link } from 'react-router-dom';
 
 import ProductDelete from './ProductDelete';
+import { SkeletonList, SkeletonLi } from '../../../Config/Styles/CommonStyle';
 
 const Columns = {
   default: 4,
@@ -98,115 +99,151 @@ function ProductOfUser() {
     };
   }, [handleClose, isOpen, itemInfo]);
 
-  if (loading) {
-    return (
-      <CenterLayout>
-        <Spinner />
-      </CenterLayout>
-    );
-  }
-
   return (
     <>
-      {userUploaded && (
+      {loading ? (
         <>
-          <Masonry
-            breakpointCols={Columns}
-            className="my-masonry-grid"
-            columnClassName="my-masonry-grid_column"
-          >
-            {userUploaded &&
-              userUploaded.map(
-                ({ _id, pdCategory, pdImage, pdTitle, pdPrice, pdlikes }) => (
-                  <div className="" key={_id} style={{ position: 'relative' }}>
-                    <strong
-                      style={{
-                        position: 'absolute',
-                        top: '11px',
-                        left: '11px',
-                        display: 'inline-block',
-                        background: '#fff',
-                        fontSize: '20px',
-                        fontStyle: 'italic',
-                        paddingLeft: '5px',
-                        paddingRight: '5px',
-                      }}
-                    >
-                      #{pdCategory}
-                    </strong>
-                    <Link to={`/product/${_id}`}>
-                      <ImageHolder br="15px">
-                        <Image src={pdImage} alt={`${pdTitle} 이미지`} />
-                      </ImageHolder>
-                    </Link>
-
-                    <div
-                      className=""
-                      style={{
-                        position: 'absolute',
-                        bottom: '0px',
-                        width: '100%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        background: '#ddd',
-                        padding: '15px',
-                        boxSizing: 'border-box',
-                      }}
-                    >
-                      <strong
-                        style={{
-                          fontSize: '18px',
-                          lineHeight: '1',
-                          width: '150px',
-                          textOverflow: 'ellipsis',
-                          whiteSpace: 'nowrap',
-                          overflow: 'hidden',
-                        }}
+          <SkeletonList>
+            <SkeletonLi>
+              <Skeleton width="100%" height="100%" />
+            </SkeletonLi>
+            <SkeletonLi>
+              <Skeleton width="100%" height="100%" />
+            </SkeletonLi>
+            <SkeletonLi>
+              <Skeleton width="100%" height="100%" />
+            </SkeletonLi>
+            <SkeletonLi>
+              <Skeleton width="100%" height="100%" />
+            </SkeletonLi>
+            <SkeletonLi>
+              <Skeleton width="100%" height="100%" />
+            </SkeletonLi>
+            <SkeletonLi>
+              <Skeleton width="100%" height="100%" />
+            </SkeletonLi>
+            <SkeletonLi>
+              <Skeleton width="100%" height="100%" />
+            </SkeletonLi>
+            <SkeletonLi>
+              <Skeleton width="100%" height="100%" />
+            </SkeletonLi>
+          </SkeletonList>
+        </>
+      ) : (
+        <>
+          {userUploaded && (
+            <>
+              <Masonry
+                breakpointCols={Columns}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+              >
+                {userUploaded &&
+                  userUploaded.map(
+                    ({
+                      _id,
+                      pdCategory,
+                      pdImage,
+                      pdTitle,
+                      pdPrice,
+                      pdlikes,
+                    }) => (
+                      <div
+                        className=""
+                        key={_id}
+                        style={{ position: 'relative' }}
                       >
-                        {pdTitle}
-                      </strong>
-                      <span style={{ marginTop: '15px' }}>
-                        <strong style={{ fontSize: '17px' }}>
-                          {Number(pdPrice).toLocaleString('ko-KR')}
+                        <strong
+                          style={{
+                            position: 'absolute',
+                            top: '11px',
+                            left: '11px',
+                            display: 'inline-block',
+                            background: '#fff',
+                            fontSize: '20px',
+                            fontStyle: 'italic',
+                            paddingLeft: '5px',
+                            paddingRight: '5px',
+                          }}
+                        >
+                          #{pdCategory}
                         </strong>
-                        원
-                      </span>
-                      <span
-                        style={{
-                          marginLeft: 'auto',
-                          position: 'absolute',
-                          top: '11px',
-                          right: '16px',
-                          fontSize: '15px',
-                        }}
-                      >
-                        좋아요 {pdlikes?.length} 개
-                      </span>
-                      <button
-                        type="button"
-                        style={{
-                          position: 'absolute',
-                          bottom: '9px',
-                          right: '16px',
-                          display: 'inline-block',
-                          verticalAlign: 'top',
-                        }}
-                        onClick={() =>
-                          handleDeleteModal({ name: pdTitle, id: _id })
-                        }
-                      >
-                        <OffScreenSpan>상품삭제 버튼</OffScreenSpan>
-                        <FontAwesomeIcon
-                          icon={faTrash}
-                          style={{ fontSize: '28px', color: 'red' }}
-                        />
-                      </button>
-                    </div>
-                  </div>
-                )
-              )}
-          </Masonry>
-          {isModalOpen && <ProductDelete deleteModal={modalProps} />}
+                        <Link to={`/product/${_id}`}>
+                          <ImageHolder br="15px">
+                            <Image src={pdImage} alt={`${pdTitle} 이미지`} />
+                          </ImageHolder>
+                        </Link>
+
+                        <div
+                          className=""
+                          style={{
+                            position: 'absolute',
+                            bottom: '0px',
+                            width: '100%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            background: '#ddd',
+                            padding: '15px',
+                            boxSizing: 'border-box',
+                          }}
+                        >
+                          <strong
+                            style={{
+                              fontSize: '18px',
+                              lineHeight: '1',
+                              width: '150px',
+                              textOverflow: 'ellipsis',
+                              whiteSpace: 'nowrap',
+                              overflow: 'hidden',
+                            }}
+                          >
+                            {pdTitle}
+                          </strong>
+                          <span style={{ marginTop: '15px' }}>
+                            <strong style={{ fontSize: '17px' }}>
+                              {Number(pdPrice).toLocaleString('ko-KR')}
+                            </strong>
+                            원
+                          </span>
+                          <span
+                            style={{
+                              marginLeft: 'auto',
+                              position: 'absolute',
+                              top: '11px',
+                              right: '16px',
+                              fontSize: '15px',
+                            }}
+                          >
+                            좋아요 {pdlikes?.length} 개
+                          </span>
+                          <button
+                            type="button"
+                            style={{
+                              position: 'absolute',
+                              bottom: '9px',
+                              right: '16px',
+                              display: 'inline-block',
+                              verticalAlign: 'top',
+                            }}
+                            onClick={() =>
+                              handleDeleteModal({ name: pdTitle, id: _id })
+                            }
+                          >
+                            <OffScreenSpan>상품삭제 버튼</OffScreenSpan>
+                            <FontAwesomeIcon
+                              icon={faTrash}
+                              style={{ fontSize: '28px', color: 'red' }}
+                            />
+                          </button>
+                        </div>
+                      </div>
+                    )
+                  )}
+              </Masonry>
+              {isModalOpen && <ProductDelete deleteModal={modalProps} />}
+            </>
+          )}
         </>
       )}
     </>
