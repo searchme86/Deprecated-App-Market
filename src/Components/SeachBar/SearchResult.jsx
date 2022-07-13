b68e37399c0f40b92bf1e1c7fd7bad09b657a28a
searchme86 <searchme86@gmail.com>
2022-07-13 16:35:46 +0900

    ⚡️ 상품검색 서치바 1차 완성

--- src/Components/SeachBar/SearchResult.jsx
+++ src/Components/SeachBar/SearchResult.jsx
@@ -1,19 +1,27 @@
-// import { Link } from 'react-router-dom';
-import { TvShowContainer, Thumbnail, Name, Rating } from './SearchBox.style';
+import {
+  Name,
+  Rating,
+  SearchItem,
+  SearchItemInfo,
+  SearchItemLink,
+} from './SearchBox.style';
+import { ImageHolder, Image } from '../../Assets/Styles/Image.style';
 
 function SearchResult(props) {
   const { thumbanilSrc, name, rating } = props;
 
   return (
-    <TvShowContainer>
-      <Thumbnail>
-        <img src={thumbanilSrc} alt={'썸네일'} />
-      </Thumbnail>
-      <Name>{name}</Name>
-      <Rating>{rating || 'N/A'}</Rating>
-      {/* <Link to={}>
-      </Link> */}
-    </TvShowContainer>
+    <SearchItem>
+      <SearchItemLink to={'/'}>
+        <ImageHolder width="100px" height="100px" mr="10px">
+          <Image src={thumbanilSrc} alt={'썸네일'} />
+        </ImageHolder>
+        <SearchItemInfo>
+          <Name>{name}</Name>
+          <Rating>{rating || 'N/A'}</Rating>
+        </SearchItemInfo>
+      </SearchItemLink>
+    </SearchItem>
   );
 }
 
