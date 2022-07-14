40c86944b634f7466a974623f86c7a6a389d18f4
searchme86 <searchme86@gmail.com>
2022-07-14 17:37:52 +0900

    🗑️  불필요한 코드 삭제

--- src/Config/Layout.jsx
+++ src/Config/Layout.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Page, PageContainer } from '../Assets/Styles/Layout.style';
-import Header from '../Components/Header';
 import { ToastContainer } from 'react-toastify';
-// import Navbar from '../Components/Header/Nav/Navbar';
-// import SearchBox from '../Components/SeachBar/SearchBox';
-// import Slider from '../Components/Slider/Slider';
+
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from '../Pages/Footer';
 import Hheader from '../Components/Hheader';
 
 function Layout() {
   return (
-    // <div className="App">
-    //   {/* <SearchBox />
-    //   <Navbar />
-    //   <Slider autoPlay={2} /> */}
-    //   <main>
-    //     <Outlet />
-    //   </main>
-    // </div>
     <Page>
-      <Header />
       <Hheader />
       <PageContainer>
         <Outlet />
