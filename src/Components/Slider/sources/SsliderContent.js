382468094a2636500c6c11ac6762109da0f95017
searchme86 <searchme86@gmail.com>
2022-07-15 23:29:39 +0900

    ♿️ 슬라이드 이미지, 웹접근성 맞게 이미지 alt 넣을 수 있게 객체에 alt 속성 추가

--- src/Components/Slider/sources/SsliderContent.js
+++ src/Components/Slider/sources/SsliderContent.js
@@ -5,14 +5,17 @@ import Img3 from './image-3.jpeg';
 export const sliderItems = [
   {
     id: 0,
+    alt: '첫번째 슬라이드 이미지',
     src: `${Img1}`,
   },
   {
     id: 1,
+    alt: '두번째 슬라이드 이미지',
     src: `${Img2}`,
   },
   {
     id: 2,
+    alt: '세번째 슬라이드 이미지',
     src: `${Img3}`,
   },
 ];
