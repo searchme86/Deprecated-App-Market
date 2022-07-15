078e5fa88d0ef5b9f12718db40074fb9cdae5b79
searchme86 <searchme86@gmail.com>
2022-07-15 10:44:17 +0900

    ⚡️ slider dot에 key 부여

--- src/Components/Slider/Dots.jsx
+++ src/Components/Slider/Dots.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { DotWrapper, Dot } from './Slider.style.js';
+import SliderStop from './SliderStop';
+import SliderPlay from './SliderPlay';
 
 function Dots(props) {
-  const { slides, activeIndex } = props;
+  const { slides, activeIndex, handlePlay, handleStop } = props;
   return (
     <DotWrapper>
-      {slides.map((slide, i) => (
-        <Dot key={slide.id} active={activeIndex === i} />
-      ))}
+      <SliderStop handleStop={handleStop} />
+      <ol>
+        {slides.map((slide, i) => (
+          <li key={i}>
+            <Dot key={slide.id} active={activeIndex === i} />
+          </li>
+        ))}
+      </ol>
+      <SliderPlay handlePlay={handlePlay} />
     </DotWrapper>
   );
 }
