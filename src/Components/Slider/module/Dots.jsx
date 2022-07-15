a6b3b5bedaea25352e03580350ff9cdad4bf8cfe
searchme86 <searchme86@gmail.com>
2022-07-15 23:25:39 +0900

    ⚰️ 필요없는 코드 삭제

--- src/Components/Slider/module/Dots.jsx
+++ src/Components/Slider/module/Dots.jsx
@@ -4,18 +4,14 @@ import SliderStop from '../play/SliderStop';
 import SliderPlay from '../play/SliderPlay';
 
 function Dots(props) {
-  const { slides, activeIndex, handlePlay, handleStop, clickDot } = props;
+  const { slides, activeIndex, handlePlay, handleStop } = props;
   return (
     <DotWrapper>
       <SliderStop handleStop={handleStop} />
       <DotList>
         {slides.map((slide, i) => (
           <DotLi key={slide.id}>
-            <Dot
-              key={slide.id}
-              active={activeIndex === i}
-              onClick={() => clickDot(i)}
-            />
+            <Dot key={slide.id} active={activeIndex === i} />
           </DotLi>
         ))}
       </DotList>
