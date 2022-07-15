1b81c45377e638472eaa13a953aba464e37bdca4
searchme86 <searchme86@gmail.com>
2022-07-15 23:40:48 +0900

    ♿️ 웹접근성에 따라 숨김 제목 텍스트 마크업 태그 변경 및 텍스트명 변경

--- src/Components/Slider/play/SliderStop.jsx
+++ src/Components/Slider/play/SliderStop.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Stop } from '../sources/Slider.style.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePause } from '@fortawesome/free-solid-svg-icons';
-import { OffScreenSpan } from '../../../Assets/Styles/Basic.style.js';
+import { OffScreenStrong } from '../../../Assets/Styles/Basic.style.js';
 
 function SliderStop({ handleStop }) {
   return (
     <Stop onClick={handleStop}>
-      <OffScreenSpan>슬라이드 자동재생 중지</OffScreenSpan>
+      <OffScreenStrong>슬라이드 자동재생 버튼</OffScreenStrong>
       <FontAwesomeIcon
         icon={faCirclePause}
         style={{ fontSize: '30px', color: '#d6de00' }}
