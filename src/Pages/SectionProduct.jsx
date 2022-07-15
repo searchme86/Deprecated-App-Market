9b947f624e9e805fa45d0b9f245c0b503b2bda6e
searchme86 <searchme86@gmail.com>
2022-07-15 10:49:50 +0900

    🎨  컴포넌트 shuffle 기능 삭제

--- src/Pages/SectionProduct.jsx
+++ src/Pages/SectionProduct.jsx
@@ -14,19 +14,20 @@ import {
 } from '../Store/Features/NProductSlice';
 import CardProduct from '../Components/CardProduct/CardProduct';
 import { OffScreenTitle } from '../Assets/Styles/Basic.style';
+import { Skeleton } from '@chakra-ui/react';
 
-const shuffle = (arr) => {
-  if (!arr && !arr.length > 0) return;
-  let currentIndex = arr.length;
-  while (0 !== currentIndex) {
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    let temporary = arr[currentIndex];
-    arr[currentIndex] = arr[randomIndex];
-    arr[randomIndex] = temporary;
-  }
-  return arr.splice(0, 8);
-};
+// const shuffle = (arr) => {
+//   if (!arr && !arr.length > 0) return;
+//   let currentIndex = arr.length;
+//   while (0 !== currentIndex) {
+//     let randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex -= 1;
+//     let temporary = arr[currentIndex];
+//     arr[currentIndex] = arr[randomIndex];
+//     arr[randomIndex] = temporary;
+//   }
+//   return arr.splice(0, 8);
+// };
 
 function SectionProduct() {
   const {
@@ -39,10 +40,10 @@ function SectionProduct() {
     dispatch(fetchAllProducts({}));
   }, []);
 
-  let shuffledItems = savedProducts && shuffle(Object.values(savedProducts));
+  // let shuffledItems = savedProducts && shuffle(Object.values(savedProducts));
 
   return (
-    <SectionUnit color="#FBE8A6">
+    <SectionUnit>
       <SectionLayout>
         <SectionHeader>
           <OffScreenTitle>신규 등록 상품</OffScreenTitle>
@@ -57,13 +58,16 @@ function SectionProduct() {
                 marginTop: '10px',
               }}
             >
-              {shuffledItems
+              {/* {shuffledItems
                 ? shuffledItems.map((item) => (
                     <CardProduct key={item._id} {...item} />
                   ))
                 : savedProducts.map((item) => (
                     <CardProduct key={item._id} {...item} />
-                  ))}
+                  ))} */}
+              {savedProducts.map((item) => (
+                <CardProduct key={item._id} {...item} />
+              ))}
             </ul>
           </div>
         </SectionContent>
