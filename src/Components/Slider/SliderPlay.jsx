3168a6062ab3a5bf986906a3a8643417c2bcfde3
searchme86 <searchme86@gmail.com>
2022-07-15 10:46:02 +0900

    🎨 slider 재생/정지 영역 폰트어썸 아이콘으로 변경

--- src/Components/Slider/SliderPlay.jsx
+++ src/Components/Slider/SliderPlay.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Play } from './Slider.style.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { OffScreenSpan } from '../../Assets/Styles/Basic.style.js';
 
-function SliderPlay({ handlePlay, handleStop }) {
+function SliderPlay({ handlePlay }) {
   return (
-    <>
-      <Play onClick={handlePlay}>play</Play>
-      {/* <Stop onClick={handleStop}>stop</Stop> */}
-    </>
+    <Play onClick={handlePlay}>
+      <OffScreenSpan>슬라이드 자동재생</OffScreenSpan>
+      <FontAwesomeIcon
+        icon={faCirclePlay}
+        style={{ fontSize: '30px', color: '#303C6C' }}
+      />
+    </Play>
   );
 }
 
