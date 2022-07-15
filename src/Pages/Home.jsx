e784366548f8dbe213ca292cfaad12487d6fa0c5
searchme86 <searchme86@gmail.com>
2022-07-15 10:47:59 +0900

    ⚡️ 홈 컴포넌트에 슬라이드 컴포넌트 추가

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,10 +4,12 @@ import ManageCategory from './Manager/category/ManageCategory';
 import SectionProduct from './SectionProduct';
 import SectionMain from './SectionMain';
 import SectionAd from './SectionAd';
+import SectionSlider from './SectionSlider';
 
 function Home() {
   return (
     <>
+      <SectionSlider />
       <SectionMain />
       <ManageCategory />
       <SectionProduct />
