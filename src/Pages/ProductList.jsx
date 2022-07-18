c2b9ae7138dd505e3d5d3410e519142e4891f1b2
searchme86 <searchme86@gmail.com>
2022-07-18 11:31:24 +0900

    🗑️  전체상품 보기 컴포넌트 주석처리된 코드 삭제

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SectionContent,
   SectionLayout,
@@ -6,10 +6,6 @@ import {
   SectionUnit,
 } from './Manager/Product/ProductUpload.style';
 
-import { Select, FormLabel } from '@chakra-ui/react';
-
-import { toast } from 'react-toastify';
-import { useForm } from 'react-hook-form';
 import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -20,11 +16,11 @@ import {
 
 import Pagination from '../Components/Pagination';
 import CardProduct from '../Components/CardProduct/CardProduct';
+import { ProductLayOut } from './SectionProduct.style';
 
 function ProductList() {
-  const [change, setChange] = useState('');
   const {
-    product: { nproducts, loading, currentPage, numberOfPages },
+    product: { savedProducts, loading, currentPage, numberOfPages },
   } = useSelector(ProductSelector);
 
   const useQuery = () => {
@@ -39,46 +35,30 @@ function ProductList() {
     dispatch(ngetProducts(currentPage));
   }, [dispatch, currentPage]);
 
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm();
-
-  const changeCategory = (e) => {
-    setChange(e.target.value);
-  };
-
   return (
     <SectionUnit>
       <SectionLayout>
         <SectionTitle>신규 상품 둘러보기</SectionTitle>
         <SectionContent>
-          <div className="">
-            <ul
-              style={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                width: '100%',
-                marginTop: '10px',
-              }}
-            >
-              {nproducts &&
-                nproducts.map((item) => (
+          {savedProducts.length > 0 ? (
+            <ProductLayOut>
+              {savedProducts &&
+                savedProducts.map((item) => (
                   <CardProduct key={item._id} {...item} />
                 ))}
-            </ul>
-          </div>
-          <div className="">
-            {nproducts?.length > 0 && !searchQuery && (
-              <Pagination
-                setCurrentPage={setCurrentPage}
-                numberOfPages={numberOfPages}
-                currentPage={currentPage}
-                dispatch={dispatch}
-              />
-            )}
-          </div>
+            </ProductLayOut>
+          ) : (
+            <p>현재 등록된 상품이 없습니다.</p>
+          )}
+
+          {savedProducts?.length > 0 && !searchQuery && (
+            <Pagination
+              setCurrentPage={setCurrentPage}
+              numberOfPages={numberOfPages}
+              currentPage={currentPage}
+              dispatch={dispatch}
+            />
+          )}
         </SectionContent>
       </SectionLayout>
     </SectionUnit>
