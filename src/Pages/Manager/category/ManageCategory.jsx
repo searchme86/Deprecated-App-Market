bc84ffb9e75b21326f976a9bd3e5e2235a696b91
searchme86 <searchme86@gmail.com>
2022-07-19 11:31:35 +0900

    ⚡️ category state를 createSelector로 가져오기

--- src/Pages/Manager/category/ManageCategory.jsx
+++ src/Pages/Manager/category/ManageCategory.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import CategoryView from './CategoryView';
-import { getCategoryList } from '../../../Store/Features/CategorySlice';
+import {
+  getCategoryList,
+  CategorySelector,
+} from '../../../Store/Features/CategorySlice';
 import { toast } from 'react-toastify';
 import { SectionLayout, SectionUnit } from '../Product/ProductUpload.style';
 import { OffScreenTitle } from '../../../Assets/Styles/Basic.style';
 
 function ManageCategory() {
-  const { categories } = useSelector((state) => state.category);
+  const {
+    category: { categories },
+  } = useSelector(CategorySelector);
   console.log('현재categories ', categories);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
