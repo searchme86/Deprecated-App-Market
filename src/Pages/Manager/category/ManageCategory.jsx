b79d554ca6568443a6daf407e4aa6c7bba91ceb2
searchme86 <searchme86@gmail.com>
2022-07-19 11:24:39 +0900

    🗑️  불필요한 코드 삭제

--- src/Pages/Manager/category/ManageCategory.jsx
+++ src/Pages/Manager/category/ManageCategory.jsx
@@ -1,18 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import CategoryView from './CategoryView';
-import { SectionTitle } from '../../../Assets/Styles/Text.style.js';
-import {
-  SectionContainer,
-  SectionDivier,
-} from '../../../Assets/Styles/Layout.style.js';
 import { getCategoryList } from '../../../Store/Features/CategorySlice';
 import { toast } from 'react-toastify';
-import {
-  SectionContent,
-  SectionLayout,
-  SectionUnit,
-} from '../Product/ProductUpload.style';
+import { SectionLayout, SectionUnit } from '../Product/ProductUpload.style';
 import { OffScreenTitle } from '../../../Assets/Styles/Basic.style';
 
 function ManageCategory() {
@@ -28,9 +19,7 @@ function ManageCategory() {
     <SectionUnit>
       <SectionLayout>
         <OffScreenTitle>카테고리별 상품찾기</OffScreenTitle>
-        <SectionContent>
-          <CategoryView categories={categories} />
-        </SectionContent>
+        <CategoryView categories={categories} />
       </SectionLayout>
     </SectionUnit>
   );
